5dcaf9956553609b536009a754e953171546934c
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-01 00:21:29 +0600

    typescript basic data type

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,27 @@
+// Basic Data Type
+
+// string
+let firstName : string = 'mahdi'
+// number
+let roll: number = 123
+// boolean
+let isAdmin: boolean = true
+// undefined
+let x: undefined = undefined
+// null
+let y: null = null
+
+
+let d: number;
+d = 123
+
+// Array
+let friends: string[] = ['mahdi', 'hassan']
+
+
+let eligibleRollList: number[] = [1, 2, 3]
+eligibleRollList.push(4)
+
+// tuple --> array --> order --> type of values
+let coordinates: [number, number] = [1, 2]
+let ageName: [number, string, boolean] = [50, 'mahdi', true]
