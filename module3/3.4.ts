248a16c8cf3682cc96d2b9dc4744cb7fc000a40d
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-06 14:19:32 +0600

    typescript: Type Grard Using Instance Of

---
+++ module3/3.4.ts
@@ -0,0 +1,68 @@
+{
+    // instance of guard
+
+    class Animal{
+        name: string;
+        species: string;
+
+        constructor(name: string, species: string){
+            this.name = name;
+            this.species = species;
+        }
+
+        makeSound(){
+            console.log(`I am making sound`)
+        }
+    }
+
+
+    class Dog extends Animal{
+        constructor(name: string, species: string){
+            super(name, species)
+        }
+        makeBark(){
+            console.log(`I am barking`)
+        }
+    }
+
+    class Cat extends Animal{
+        constructor(name: string, species: string){
+            super(name, species)
+        }
+        makeMeaw(){
+            console.log(`I am mewaing`)
+        }
+    }
+
+
+    // smart way te handle korar jonno chaile amra function bebohar korte pari
+    const isDog =(animal : Animal): animal is Dog =>{
+        return animal instanceof Dog
+    }
+
+    const isCat = (animal : Animal) : animal is Cat => {
+        return animal instanceof Cat
+    }
+
+
+
+    const getAnimal = (animal: Animal) => {
+        if(isDog(animal)){
+            animal.makeBark()
+        }
+        else if(isCat(animal)){
+            animal.makeMeaw()
+        }
+        else{
+            animal.makeSound()
+        }
+    }
+
+    const dog = new Dog('Dog Bhai', 'dog')
+    const cat = new Cat('Cat Bhai', 'cat')
+
+
+    getAnimal(dog)
+    getAnimal(cat)
+
+}
